631d7066cb4ccc383aeac4cdfbee9ecb47f817c6
Roman Pogorelov <rmk140987@gmail.com>
2021-09-07 16:03:56 +0300

    1. Servlet. Web.xml [#6866]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -6,12 +6,18 @@ import ru.job4j.dream.model.Post;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
     private static final Store INST = new Store();
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    public static Store instOf() {
+        return INST;
+    }
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
@@ -22,8 +28,9 @@ public class Store {
         candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
-    public static Store instOf() {
-        return INST;
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 
     public Collection<Post> findAllPosts() {
